015ce1cbc3b35798c3b757b8feaede92c8201d44
Pierre Schmitz <pierre@archlinux.de>
2006-01-18 12:07:58 +0000

    some corrections

--- pages/DeleteUser.php
+++ pages/DeleteUser.php
@@ -16,9 +16,14 @@ protected function setForm()
 		$this->showFailure('Kein Benutzer angegeben');
 		}
 
-	if (!$this->User->isOnline() && ($this->User->getID() != $this->user || !$this->User->isAdmin()))
+	if (!$this->User->isOnline())
 		{
 		$this->showFailure('Nur für Mitglieder!');
+		}
+
+	if ($this->User->getID() != $this->user && !$this->User->isAdmin())
+		{
+		$this->showFailure('Keine Berechtigung!');
 		}
 
 	$this->setValue('title', 'Benutzerkonto löschen');
@@ -42,94 +47,96 @@ protected function setForm()
 		$this->showFailure('Kein Benutzer gefunden');
 		}
 
+	$this->addHidden('user', $this->user);
+
 	$this->addElement('hint', 'Soll das Benutzerkonto von <strong>'.$username.'</strong> wirklich gelöscht werden?');
 
 	$this->addRadio('confirm', 'Bestätige Deine Entscheidung',
-	array('Ja, ich möchte dieses Benutzerkonto endgültig löschen.' => 1, 'Nein, lieber doch nichts löschen.' => 0), 0);
-	$this->setLength('gender', 1, 1);
+	array('Ja, ich möchte dieses Benutzerkonto endgültig löschen.' => 1, 'Nein, lieber doch nichts löschen.' => 2), 2);
+	$this->requires('confirm');
+	$this->setLength('confirm', 1, 1);
 	}
 
 protected function checkForm()
 	{
-	if (!$this->User->isOnline() && ($this->User->getID() != $this->user || !$this->User->isAdmin()))
-		{
-		$this->showWarning('Na, was hattest Du denn vor?');
-		}
 	}
 
 protected function sendForm()
 	{
-	$this->Sql->query
-		('
-		DELETE FROM
-			users
-		WHERE
-			id = '.$this->user
-		);
-
-	$this->Sql->query
-		('
-		DELETE FROM
-			poll_voters
-		WHERE
-			userid = '.$this->user
-		);
-/*
-	$this->Sql->query
-		('
-		DELETE FROM
-			files
-		WHERE
-			userid = '.$this->user
-		);
-*/
-	$this->Sql->query
-		('
-		DELETE FROM
-			thread_user
-		WHERE
-			userid = '.$this->user
-		);
-
-	$this->Sql->query
-		('
-		DELETE FROM
-			threads_log
-		WHERE
-			userid = '.$this->user
-		);
-
-	$this->Sql->query
-		('
-		DELETE FROM
-			user_group
-		WHERE
-			userid = '.$this->user
-		);
-
-	$this->Sql->query
-		('
-		UPDATE
-			threads
-		SET
-			firstuserid = 0
-		WHERE
-			firstuserid = '.$this->user
-		);
-
-	$this->Sql->query
-		('
-		UPDATE
-			threads
-		SET
-			lastuserid = 0
-		WHERE
-			lastuserid = '.$this->user
-		);
-
-	if ($this->user == $this->User->getId())
+	if ($this->Io->getInt('confirm') == 1)
 		{
-		$this->User->logout();
+		$this->Sql->query
+			('
+			DELETE FROM
+				users
+			WHERE
+				id = '.$this->user
+			);
+
+		$this->Sql->query
+			('
+			DELETE FROM
+				poll_voters
+			WHERE
+				userid = '.$this->user
+			);
+	/*
+		$this->Sql->query
+			('
+			DELETE FROM
+				files
+			WHERE
+				userid = '.$this->user
+			);
+	*/
+		$this->Sql->query
+			('
+			DELETE FROM
+				thread_user
+			WHERE
+				userid = '.$this->user
+			);
+
+		$this->Sql->query
+			('
+			DELETE FROM
+				threads_log
+			WHERE
+				userid = '.$this->user
+			);
+
+		$this->Sql->query
+			('
+			DELETE FROM
+				user_group
+			WHERE
+				userid = '.$this->user
+			);
+
+		$this->Sql->query
+			('
+			UPDATE
+				threads
+			SET
+				firstuserid = 0
+			WHERE
+				firstuserid = '.$this->user
+			);
+
+		$this->Sql->query
+			('
+			UPDATE
+				threads
+			SET
+				lastuserid = 0
+			WHERE
+				lastuserid = '.$this->user
+			);
+
+		if ($this->user == $this->User->getId())
+			{
+			$this->User->logout();
+			}
 		}
 
 	$this->Io->redirect('Forums');
