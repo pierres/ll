2f238bba55b827f3463c6513456373dc606696e7
Pierre Schmitz <pierre@archlinux.de>
2006-01-22 09:16:18 +0000

    Exceptions have correct XHTML now

--- modules/Exceptions.php
+++ modules/Exceptions.php
@@ -9,8 +9,8 @@ function ExceptionHandler(Exception $e)
 	$screen = '<?xml version="1.0" encoding="UTF-8" ?>
 		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "xhtml11.dtd">
 		<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 		<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 		<title>'.get_class($e).'</title>
 		</head>
 		<body>
