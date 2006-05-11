022f57ebde19761102cefba2473c739407682613
Pierre Schmitz <pierre@archlinux.de>
2006-05-11 08:14:26 +0000

    added telephone-number to Impressum

--- pages/Impressum.php
+++ pages/Impressum.php
@@ -17,11 +17,28 @@ public function prepare()
 			</tr>
 			<tr>
 				<td class="main">
-					<a href="?page=ShowUser;id=1;user=486">Pierre Schmitz</a>
+					<a href="?page=ShowUser;id=1;user=486" class="link">Pierre Schmitz</a>
 					<p>
 					Clemens-August-Straße 76<br />
 					53115 Bonn<br /><br />
-					<a href="?page=Contact;id=1">E-Mail</a>
+					<table>
+						<tr>
+							<td style="padding-right:20px;">E-Mail</td>
+							<td><a href="?page=Contact;id=1" class="link">pschmitz&#64;laber-land.de</a></td>
+						</tr>
+						<tr>
+							<td style="padding-right:20px;">Telefon</td>
+							<td>0228&nbsp;9716608</td>
+						</tr>
+						<tr>
+							<td style="padding-right:20px;">Mobil</td>
+							<td>0160&nbsp;95269831</td>
+						</tr>
+						<tr>
+							<td style="padding-right:20px;">Jabber</td>
+							<td>pierre&#64;jabber.laber-land.de</td>
+						</tr>
+					</table>
 					</p>
 				</td>
 			</tr>
