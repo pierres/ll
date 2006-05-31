1748b421bae5657b967582d27c9cafe051017aab
Pierre Schmitz <pierre@archlinux.de>
2006-05-31 07:47:52 +0000

    added Feature #36 (no gui-frontend yet)

--- modules/AntiSpam.php
+++ modules/AntiSpam.php
@@ -20,7 +20,32 @@ public function isSpam($text)
 		$blacklist = array();
 		}
 
-	return $this->isInBlacklist($this->getDomains($text), $blacklist);
+	$evilDomains = $this->getDomainsInBlacklist($this->getDomains($text), $blacklist);
+
+	if (empty($evilDomains))
+		{
+		return false;
+		}
+
+	$stm = $this->DB->prepare
+		('
+		UPDATE
+			domain_blacklist
+		SET
+			counter = counter + 1,
+			lastmatch = UNIX_TIMESTAMP()
+		WHERE
+			domain = ?
+		');
+
+	foreach ($evilDomains as $evilDomain)
+		{
+		$stm->bindString($evilDomain);
+		$stm->execute();
+		}
+	$stm->close();
+
+	return true;
 	}
 
 public function addSpam($text)
@@ -45,7 +70,9 @@ public function addSpam($text)
 		INSERT INTO
 			domain_blacklist
 		SET
-			domain = ?
+			domain = ?,
+			inserted = UNIX_TIMESTAMP(),
+			lastmatch = UNIX_TIMESTAMP()
 		');
 	foreach (array_diff($this->getDomains($text), $blacklist) as $domain)
 		{
@@ -60,26 +87,28 @@ private function getDomains($text)
 	{
 	$protocoll 	= '(?:https?|ftp):\/\/';
 	$name 		= '[a-z0-9](?:[a-z0-9_\-\.]*[a-z0-9])?';
-	$tld 		= '[a-z]{2,5}';
+	$tld 			= '[a-z]{2,5}';
 	$domain		=  $name.'\.'.$tld;
-// 	$address	= '(?:'.$domain.'|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})';
+// 	$address		= '(?:'.$domain.'|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})';
 
 	preg_match_all('/(?:'.$protocoll.'(?:www\.)?|www\.)('.$domain.')/', $text, $domains);
 
 	return array_unique($domains[1]);
 	}
 
-private function isInBlacklist($domains, $blacklist)
+private function getDomainsInBlacklist($domains, $blacklist)
 	{
+	$matches =array();
+
 	foreach ($domains as $domain)
 		{
 		if (in_array($domain, $blacklist))
 			{
-			return true;
+			$matches[] = $domain;
 			}
 		}
 
-	return false;
+	return $matches;
 	}
 
 
