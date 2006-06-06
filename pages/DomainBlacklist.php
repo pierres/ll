93c95fa3cbf95fa433a83abdeac31dc8d6e19efc
Pierre Schmitz <pierre@archlinux.de>
2006-06-06 20:29:11 +0000

    gui-fixes

--- pages/DomainBlacklist.php
+++ pages/DomainBlacklist.php
@@ -18,7 +18,7 @@ public function prepare()
 				</td>
 			</tr>
 			<tr>
-				<td class="main" style="width:1000px;">
+				<td class="main" style="width:1100px;">
 					<p>Leider treten in letzter Zeit vermehrt Probleme mit Werbung auf, die automatisiert in Foren, Wikis und Gästebüchern veröffentlicht wird. Um diesem <a href="http://de.wikipedia.org/wiki/Spam#Wiki-.2C_Link-_und_Blogspam" class="extlink" onclick="return !window.open(this.href);" rel="nofollow">Spam</a> entgegen zu wirken, werden wir Domains, für die Werbung gemacht wird sperren.</p>
 					<p>Bisher wurden '.$stat['counts'].' Spam-Versuche für '.$stat['domains'].' Domains blockiert. Sollte eine Domain fälschlicherweise auf dieser Liste stehen, so <a href="?page=Contact;id='.$this->Board->getId().'" class="link">teile uns dies bitte mit</a>.</p>
 					'.$this->getDomainBlacklist().'
@@ -75,7 +75,7 @@ private function getDomainBlacklist()
 			$domains = array();
 			}
 
-		$list = '<table style="margin:10px;width:900px;">
+		$list = '<table style="margin:10px;width:1050px;">
 				<tr>
 					<td style="padding-bottom:5px;padding-right:200px;;"><strong>Domain</strong></td>
 					<td style="text-align:right;padding-bottom:5px;"><strong>Spam-Versuche</strong></td>
