2fefa3a6741ce020996c67df9656fe15559b7c20
Pierre Schmitz <pierre@archlinux.de>
2006-06-06 16:29:42 +0000

    only users are allowed to view the blocked domains

--- pages/DomainBlacklist.php
+++ pages/DomainBlacklist.php
@@ -21,15 +21,7 @@ public function prepare()
 				<td class="main" style="width:850px;">
 					<p>Leider treten in letzter Zeit vermehrt Probleme mit Werbung auf, die automatisiert in Foren, Wikis und Gästebüchern veröffentlicht wird. Um diesem <a href="http://de.wikipedia.org/wiki/Spam#Wiki-.2C_Link-_und_Blogspam" class="extlink" onclick="return !window.open(this.href);" rel="nofollow">Spam</a> entgegen zu wirken, werden wir Domains, für die Werbung gemacht wird sperren.</p>
 					<p>Bisher wurden '.$stat['counts'].' Spam-Versuche für '.$stat['domains'].' Domains blockiert. Sollte eine Domain fälschlicherweise auf dieser Liste stehen, so <a href="?page=Contact;id='.$this->Board->getId().'" class="link">teile uns dies bitte mit</a>.</p>
-					<table style="margin:10px;width:800px;">
-						<tr>
-							<td style="padding-bottom:5px;padding-right:100px;;"><strong>Domain</strong></td>
-							<td style="text-align:right;padding-bottom:5px;"><strong>Spam-Versuche</strong></td>
-							<td style="text-align:right;padding-bottom:5px;padding-right:100px;"><strong>eingefügt</strong></td>
-							<td style="text-align:right;padding-bottom:5px;"><strong>zuletzt blockiert</strong></td>
-						</tr>
-						'.$this->getDomainBlacklist().'
-					</table>
+					'.$this->getDomainBlacklist().'
 				</td>
 			</tr>
 		</table>
@@ -61,33 +53,48 @@ private function getStat()
 
 private function getDomainBlacklist()
 	{
-	try
+	if ($this->User->isOnline())
 		{
-		$domains = $this->DB->getRowSet
-			('
-			SELECT
-				domain,
-				counter,
-				inserted,
-				lastmatch
-			FROM
-				domain_blacklist
-			ORDER BY
-				lastmatch DESC
-			');
-		}
-	catch (DBNoDataException $e)
-		{
-		$domains = array();
-		}
+		try
+			{
+			$domains = $this->DB->getRowSet
+				('
+				SELECT
+					domain,
+					counter,
+					inserted,
+					lastmatch
+				FROM
+					domain_blacklist
+				ORDER BY
+					lastmatch DESC
+				');
+			}
+		catch (DBNoDataException $e)
+			{
+			$domains = array();
+			}
 
-	$list = '';
-	foreach ($domains as $domain)
+		$list = '<table style="margin:10px;width:800px;">
+				<tr>
+					<td style="padding-bottom:5px;padding-right:100px;;"><strong>Domain</strong></td>
+					<td style="text-align:right;padding-bottom:5px;"><strong>Spam-Versuche</strong></td>
+					<td style="text-align:right;padding-bottom:5px;padding-right:100px;"><strong>eingefügt</strong></td>
+					<td style="text-align:right;padding-bottom:5px;"><strong>zuletzt blockiert</strong></td>
+				</tr>';
+		foreach ($domains as $domain)
+			{
+			$list .= '<tr><td style="padding-right:100px;">'.$domain['domain'].'</td>
+					<td style="text-align:center;">'.$domain['counter'].'</td>
+					<td style="text-align:right;padding-right:100px;">'.formatDate($domain['inserted']).'</td>
+					<td style="text-align:right;">'.formatDate($domain['lastmatch']).'</td></tr>';
+			}
+
+		$list .= '</table>';
+		}
+	else
 		{
-		$list .= '<tr><td style="padding-right:100px;">'.$domain['domain'].'</td>
-				<td style="text-align:center;">'.$domain['counter'].'</td>
-				<td style="text-align:right;padding-right:100px;">'.formatDate($domain['inserted']).'</td>
-				<td style="text-align:right;">'.formatDate($domain['lastmatch']).'</td></tr>';
+		$list = '<p>Um die Liste der gesperrten Domains hier einzusehen, muß Du Dich <a href="?page=Register;id='.$this->Board->getId().'" class="link">registrieren</a> und <a href="?page=Login;id='.$this->Board->getId().'" class="link">anmelden</a>.';
 		}
 
 	return $list;
