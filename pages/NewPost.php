dca6efc0112b6fff0f4e7db4a260dbe643cea063
Pierre Schmitz <pierre@archlinux.de>
2006-06-07 06:31:55 +0000

    added more usability to AntiSpam-Protection

--- pages/NewPost.php
+++ pages/NewPost.php
@@ -434,7 +434,7 @@ protected function checkForm()
 		if ($AntiSpam->isSpam())
 			{
 			unset($AntiSpam);
-			$this->showFailure('Dein Beitrag wurde als Spam eingestuft. Falls dies eine Falschmeldung ist, <a href="?page=Contact;id='.$this->Board->getId().'" class="link">benachrichtige uns</a> bitte. Siehe auch: <a href="?page=DomainBlacklist;id='.$this->Board->getId().'" class="link">Gesperrte Domains</a>');
+			$this->showWarning('Dein Beitrag wurde als Spam eingestuft. Falls dies eine Falschmeldung ist, <a href="?page=Contact;id='.$this->Board->getId().'" class="link">benachrichtige uns</a> bitte. Siehe auch: <a href="?page=DomainBlacklist;id='.$this->Board->getId().'" class="link">Gesperrte Domains</a>');
 			}
 		}
 
