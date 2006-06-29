0a2ad49439256df322af48e3a4372e05e6b3b0b3
Pierre Schmitz <pierre@archlinux.de>
2006-06-29 19:51:25 +0000

    and what about this?

--- modules/Mail.php
+++ modules/Mail.php
@@ -11,7 +11,7 @@ private $subject 	= '';
 
 public function send()
 	{
-	mail($this->to, $this->subject, $this->text, 'Content-Type: text/plain;charset="UTF-8"'."\r\n".'From: '.$this->from."\r\n");
+	mb_send_mail($this->to, $this->subject, $this->text, 'Content-Type: text/plain;charset="UTF-8"'."\r\n".'From: '.$this->from."\r\n");
 	}
 
 public function setFrom($from)
@@ -37,53 +37,10 @@ public function setText($text)
 public function validateMail($mail)
 	{
 	$name 		= '[a-z0-9](?:[a-z0-9_\-\.]*[a-z0-9])?';
-	$tld 			= '[a-z]{2,5}';
+	$tld 		= '[a-z]{2,5}';
 	$domain		=  $name.'\.'.$tld;
 
 	return (boolean) preg_match('/^'.$name.'@'.$domain.'$/i', $mail);
-	}
-
-private function encodeMimeSubject($s)
-	{
-	$lastspace=-1;
-	$r="";
-	$buff="";
-
-	$mode=1;
-
-	for ($i=0; $i<strlen($s); $i++) {
-		$c=substr($s,$i,1);
-		if ($mode==1) {
-		$n=ord($c);
-		if ($n & 128) {
-			$r.="=?UTF-8?Q?";
-			$i=$lastspace;
-			$mode=2;
-		} else {
-			$buff.=$c;
-			if ($c==" ") {
-			$r.=$buff;
-			$buff="";
-			$lastspace=$i;
-			}
-		}
-		} else if ($mode==2) {
-		$r.=$this->qpchar($c);
-		}
-	}
-	if ($mode==2) $r.="?=";
-
-	return $r;
-	}
-
-private function qpchar($c)
-	{
-	$n=ord($c);
-	if ($c==" ") return "_";
-	if ($n>=48 && $n<=57) return $c;
-	if ($n>=65 && $n<=90) return $c;
-	if ($n>=97 && $n<=122) return $c;
-	return "=".($n<16 ? "0" : "").strtoupper(dechex($n));
 	}
 
 }
