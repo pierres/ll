434fdec085dce0eae2b57ffab1a4031b06fb343e
Pierre Schmitz <pierre@archlinux.de>
2007-06-19 21:15:12 +0000

    be nice

--- modules/Board.php
+++ modules/Board.php
@@ -34,7 +34,7 @@ public function __construct()
 		catch (DBNoDataException $e)
 			{
 			/**@TODO: evtl. ein schlechter Fallback */
-			die('kein Board gefunden!');
+			$board = $this->getBoard(1);
 			}
 		}
 
