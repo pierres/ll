e22bfb00478e45e5422913825963a3f9c1ca34e6
Pierre Schmitz <pierre@archlinux.de>
2009-10-13 19:59:25 +0200

    remove unneeded workaround which wasn't utf8-save anyway

--- modules/Functions.php
+++ modules/Functions.php
@@ -37,25 +37,16 @@ function unhtmlspecialchars($string)
 // see http://w3.org/International/questions/qa-forms-utf-8.html
 function is_unicode($input)
 	{
-	# long values will make pcre segfaulting...
-	foreach (str_split($input, 1000) as $value)
-		{
-		if (!preg_match('%^(?:
-			[\x09\x0A\x0D\x20-\x7E]			# ASCII
-			| [\xC2-\xDF][\x80-\xBF]		# non-overlong 2-byte
-			|  \xE0[\xA0-\xBF][\x80-\xBF]		# excluding overlongs
-			| [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}	# straight 3-byte
-			|  \xED[\x80-\x9F][\x80-\xBF]		# excluding surrogates
-			|  \xF0[\x90-\xBF][\x80-\xBF]{2}	# planes 1-3
-			| [\xF1-\xF3][\x80-\xBF]{3}		# planes 4-15
-			|  \xF4[\x80-\x8F][\x80-\xBF]{2}	# plane 16
-			)*$%xs', $value))
-			{
-			return false;
-			}
-		}
-
-	return true;
+	return preg_match('%^(?:
+		[\x09\x0A\x0D\x20-\x7E]			# ASCII
+		| [\xC2-\xDF][\x80-\xBF]		# non-overlong 2-byte
+		|  \xE0[\xA0-\xBF][\x80-\xBF]		# excluding overlongs
+		| [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}	# straight 3-byte
+		|  \xED[\x80-\x9F][\x80-\xBF]		# excluding surrogates
+		|  \xF0[\x90-\xBF][\x80-\xBF]{2}	# planes 1-3
+		| [\xF1-\xF3][\x80-\xBF]{3}		# planes 4-15
+		|  \xF4[\x80-\x8F][\x80-\xBF]{2}	# plane 16
+		)*$%xs', $input);
 	}
 
 function cutString($string, $length)
